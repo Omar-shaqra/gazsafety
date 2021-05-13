4cf04b942030594a2b1da865c23e84612f79040a
Omar-shaqra <49815667+Omar-shaqra@users.noreply.github.com>
2021-05-13 03:36:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 3000;
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const uri = "mongodb+srv://shaqra:shaqra1299@cluster0.osy09.mongodb.net/test?retryWrites=true&w=majority";
-const dbname ='test';
+const uri = "mongodb+srv://shaqra:shaqra1299@cluster0.osy09.mongodb.net/gazsafety?retryWrites=true&w=majority";
+const dbname ='gazsafety';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
