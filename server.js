63da562f0d57425a2f9924d44d739d40583a9c57
Omar-shaqra <49815667+Omar-shaqra@users.noreply.github.com>
2021-06-03 23:04:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -43,16 +43,6 @@ db.once('open', function() {
 });
 
 
-const insertDocuments = function(db, callback) {
-const collection = db.collection('test');
-collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
-   assert.equal(err, null);
-   assert.equal(3, result.result.n);
-   assert.equal(3, result.ops.length);
-   console.log('Inserted 3 documents into the collection');
-   callback(result);
- });
-};
 
 
 var nodemailer = require('nodemailer');
